73fee9dbbcfe778f75bee9e40fe7b114320f14a8
TestJeong <jyunj3015@gmail.com>
2020-05-11 23:47:41 +0900

    .

--- models/Video.js
+++ models/Video.js
@@ -30,6 +30,10 @@ const VideoSchema = new mongoose.Schema({
 const model = mongoose.model("Video", VideoSchema);
 export default model;
 
+
+// Video라는 이름으로 VideoSchema를 사용 하겠다는 말. (여기서 Video는 아무 단어든 사용 할 수 있음)
+// 데이터베이스에 들어가보면 자동으로 s를 붙혀 복수형으로 적용이 됨
+
 // ref 필드는 언급 된 ID가 검색 될 콜렉션을 의미합니다.
 // Room 스키마를 참고(ref)하여 type에 ObjectId를 넣습니다. 이로서 관계가 성립합니다.
 //reference : 참고 "Comment" 모델에서 부합하는 ID를 찾겠다는 뜻이라고 해야할까요
