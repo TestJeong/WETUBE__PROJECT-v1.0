7a9f4de037590082c629edd0e14441c7cacdf2ef
TestJeong <jyunj3015@gmail.com>
2020-06-17 23:11:06 +0900

    .

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,26 +2,59 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async () => {
+let streamObject;
+let videoRecorder;
+
+const handleVideoData = (event) => {
+  const {
+    data: videoFile
+  } = event; // blob에서 data를 받아온다 (data의 이름을 videoFile로 사용한다)
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm"
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
+
+};
+
+const startRecording = () => {
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData); //video가 정지되면 함수가 실행
+  recordBtn.addEventListener("click", stopRecording);
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
       video: {
         width: 1280,
-        height: 720
-      }
-    });
-    videoPreview.srcObject = stream;
+        height: 720,
+      }, // 제약조건을 상세하게 설정이 가능하다
+    }); // 제약 조건을 설정, 요청할 미디어 유형 
+    videoPreview.srcObject = stream; // mediaStream을 url로 처리하기위해 사용하는 것. stream은 객체이다
     videoPreview.muted = true;
     videoPreview.play();
+    recordBtn.innerHTML = "Stop recording";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
     recordBtn.innerHTML = "🔴 Cant record";
-    recordBtn.removeEventListener("clcik", startRecording);
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
-}
+};
 
 function init() {
-  recordBtn.addEventListener("click", startRecording)
+  recordBtn.addEventListener("click", getVideo);
 }
 
 if (recorderContainer) {
